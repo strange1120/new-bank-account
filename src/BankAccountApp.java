171ef59a25ae7346476600e9e2bcf1ca5c6202e3
Sarah Strange <strange1120@gmail.com>
2018-06-02 01:10:41 -0400

    finished

--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -1,18 +1,12 @@
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
 
 	public static void main(String[] args) {
+		List<Account> accounts = new LinkedList<Account>();
 
 		String file = "/Users/sarahstrange/Desktop/dev/new-bank-account/NewBankAccounts.csv";
-
-		Checking checkingAccount1 = new Checking("Tom Wilson", "738476374", 1500);
-
-		Savings savingsAccount1 = new Savings("Rich Lowe", "837485746", 2500);
-
-		savingsAccount1.showInfo();
-		System.out.println("***************");
-		checkingAccount1.showInfo();
 
 		List<String[]> newAccounts = utilities.CSV.read(file);
 		for (String[] newAccount : newAccounts) {
@@ -22,12 +16,17 @@ public class BankAccountApp {
 			double initialDeposit = Double.parseDouble(newAccount[3]);
 			System.out.println(name + " " + SSN + " " + accountType + " $" + initialDeposit);
 			if (accountType.equals("Savings")) {
-				System.out.println("Open a savings account");
+				accounts.add(new Savings(name, SSN, initialDeposit));
 			} else if (accountType.equals("Checking")) {
-				System.out.println("Open a checking account");
+				accounts.add(new Checking(name, SSN, initialDeposit));
 			} else {
 				System.out.println("Error reading account type");
 			}
+		}
+
+		for (Account account : accounts) {
+			System.out.println("\n*************");
+			account.showInfo();
 		}
 	}
 
